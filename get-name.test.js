4affcf8c1f4d4b3bb3bcebb3b93751cb75b77ae2
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-19 16:48:19 -0700

    updated test to return expected

--- get-name.test.js
+++ get-name.test.js
@@ -3,12 +3,13 @@ import getName from './get-name';
 
 describe('getName', () => {
 
-    it('gets the name of an object'), () => {
-
+    it('gets the name of an object', () => {
         const spot = { name: 'spot', age: 5, weight: '20 lbs' };
-        getName(spot); // returns spot
+        const actual = getName(spot); // returns spot
+        const expected = 'spot'; 
     // const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
     // getName(character) // returns Aang
-        
-    };
+    
+        expect(actual).toEqual(expected);
+    });
 });
