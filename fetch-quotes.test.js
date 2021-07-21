5a8b37f6c4979e3d66e0ac58af5423eba721fa75
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-21 13:32:25 -0700

    updated  fetch quote test to be specific and changed function to actually grab a single object

--- fetch-quotes.test.js
+++ fetch-quotes.test.js
@@ -3,12 +3,16 @@ import fetchQuotes from './fetch-quotes';
 
 describe('fetchQuotes', () => {
 
-    it('fetches a single quote from the futurama api', () => {
+    it('fetches a single quote from the futurama api', async () => {
 
     
-        const actual = fetchQuotes();
+        const actual = await fetchQuotes();
     
-        expect(actual).toEqual(expect.any(Object));
+        expect(actual).toEqual({
+            character: expect.any(String),
+            quote: expect.any(String),
+            image: expect.any(String)
+        });
     });
     
 });
